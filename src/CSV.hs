2bd21cfc141879693f93ab58f9c3ea450193d434
Lúcás Meier <cronokirby@gmail.com>
2018-08-19 22:13:50 +0200

    adds a splitRow function and a representation of raw csv rows

--- src/CSV.hs
+++ src/CSV.hs
@@ -3,6 +3,7 @@ This module contains functions related to parsing CSV files.
 -}
 module CSV
     ( splitLines
+    , splitRow
     )
 where
 
@@ -11,12 +12,34 @@ import Pipes
 import qualified Pipes.Prelude as P
 
 
--- | splits a string into individual lines
+data RawRow = RawRow [T.Text] deriving (Eq, Show)
+
+
+-- | Splits a string into 2 parts on the first char it finds, skipping the char
+splitWith :: Char -> T.Text -> (T.Text, T.Text)
+splitWith c txt =
+    let (this, that) = T.break (== c) txt
+        rest = T.drop 1 that
+    in (this, rest)
+
+
+-- | Splits a string into individual lines
 splitLines :: Monad m => T.Text -> Producer T.Text m ()
 splitLines txt = do
-    let (this, that) = T.break (== '\n') txt
-        rest = T.drop 1 that -- removing the newline
+    let (this, rest) = splitWith '\n' txt
     yield this
     if T.null rest
         then return ()
         else splitLines rest
+
+
+-- | Parses a bit of text by commas into a row
+splitRow :: T.Text -> RawRow
+splitRow = RawRow . reverse . go []
+  where
+    go :: [T.Text] -> T.Text -> [T.Text]
+    go acc txt
+      | T.null txt = acc
+      | otherwise  =
+        let (this, rest) = splitWith ',' txt
+        in go (this : acc) rest
